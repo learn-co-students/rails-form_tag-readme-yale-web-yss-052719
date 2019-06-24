112ea4454e29664dd7675153457cef0cde7661e5
jqzheng617 <jenna.zheng617@gmail.com>
2019-06-24 00:19:09 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,16 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new
+  end
+
+  def create
+
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+    #creates a new post
+    #it's redirecting to index page
+  end
+
 end
